22c9f9aaf8d9906e81f19da27d55760858227880
MekImEne <fi_mekideche@esi.dz>
2020-04-10 23:02:18 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <AddTodo v-on:add-todo="addTodo"></AddTodo>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"></Todos>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layouts/Header.vue";
+import Todos from "./components/ToDos";
+import AddTodo from "./components/AddTodo";
+import axios from "axios";
+export default {
+  name: "app",
+  data() {
+    return {
+      /*todos: [
+        {
+          id: 1,
+          title: "ToDo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "ToDo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "ToDo Three",
+          completed: false
+        }
+      ]*/
+      todos: []
+    };
+  },
+  components: {
+    Todos,
+    Header,
+    AddTodo
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete("https://jsonplaceholder.typicode.com/todos/${id}")
+        .then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(err => console.log(err));
+    },
+    created() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos?_limit=20")
+        .then(res => (this.todos = res.data))
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
